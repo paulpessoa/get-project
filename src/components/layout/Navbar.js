3ef2efc03990f7d6fba2dba27c0a8ecfdea70bb4
Paul Pessoa <paulmspessoa@gmail.com>
2022-04-29 00:58:56 -0300

    fix logo

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Container from "./Container";
 import styles from "./Navbar.module.css";
-import logo from "../../img/getproject-logo.png";
+import logo from "../../img/getprojectlogo.png";
 
 function Navbar() {
   return (

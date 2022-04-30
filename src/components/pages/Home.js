3205b4af368cb2f02565753e01eb8351cda701b8
Paul Pessoa <paulmspessoa@gmail.com>
2022-04-30 13:11:20 -0300

    class

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,15 @@
 import React from "react";
+import logo from "../../img/getprojectlogo.png";
+import styles from "./Home.module.css";
 
 function Home () {
     return (
-        <p>home</p>
+        <div className={styles.beleuza}>
+            <h1>Bem vindo ao <span>GetProject</span></h1>
+            <p>Comece a gerencias os seus projetos agora mesmo</p>
+            <a href='/'>Criar Projeto</a>
+            <img src={logo} alt='Saving Project' />
+        </div>
     )
 }
 

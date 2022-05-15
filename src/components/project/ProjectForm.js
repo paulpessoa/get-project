ec0e064522fb47505fe242a02c1a6db430a623d7
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-14 22:18:41 -0300

    GET com fetch

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
@@ -5,11 +7,27 @@ import styles from './ProjectForm.module.css';
 
 
 function ProjectForm ({btnText}) {
+    const [categories, setCategories] = useState ([])
+    
+    useEffect(() => {     
+        fetch('http://localhost:5000/categories',{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log("bosta"))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input type="text" text="Nome do Projeto" name="name" placeholder="Insira o nome do Projeto"/> 
             <Input type="number" text="Orçamento do Projeto" name="budget" placeholder="Insira o orçamento total"/>
-            <Select name="category_id" text="Selecione uma Categoria"/>
+            <Select name="category_id" text="Selecione uma Categoria" options={categories}/>
             <SubmitButton type="submit" text={btnText}/>
         </form>
     )

35c1190538ae3d8091c7f2d6f34c30563d3560ad
Paul Pessoa <paul.pessoa@predify.me>
2022-05-25 23:51:12 -0300

    style projects page

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -1,19 +1,28 @@
-import styles from './ProjectForm.module.css'
-import {BsPencil, BsFillTransFill} from 'react-icons/bs'
-
+import styles from './ProjectCard.module.css'
+import {BsPencil, BsFillTrashFill} from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 
 function ProjectCard ({id, name, budget, category, handleRemove}) {
     return (
-   <div>
+<div className={styles.container_cards}>
 
+   <div className={styles.project_card}>
        <h4>{name}</h4>
        <p><span>Orçamento:</span> R${budget}</p>
-       <p><span></span> {category}</p>
-        <div>
-            <p>Editar</p>
-            <p>Remover</p>
+       <p className={styles.category_text}>
+           <span className={`${styles[category.toLowerCase()]}`}></span> {category}</p>
+        <div className={styles.project_card_actions}>
+            
+            <Link to="/">
+                <BsPencil/> Editar
+            </Link>
+            <button>
+                <BsFillTrashFill/> Excluir
+            </button>
         </div>
    </div>
+   
+</div>
     )
 }
 

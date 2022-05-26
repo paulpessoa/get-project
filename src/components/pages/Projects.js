d5b0668af37916630b044af7b58bf8d9c5aa7083
Paul Pessoa <paul.pessoa@predify.me>
2022-05-25 23:10:38 -0300

    estrutura project card component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,15 +1,37 @@
 import Message from "../layout/Message";
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 import styles from './Projects.module.css'
 import Container from '../layout/Container';
 import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
+
 
 function Projects () {
+    const [projects, setProjects] = useState ([])
+
     const location = useLocation()
     let message = ''
     if(location.state) {
          message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            header: {
+                'Content-tType': 'application/json',
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
 
     return (
         <div className={styles.project_container}>
@@ -21,8 +43,19 @@ function Projects () {
            
            
             <Container customClass="start"> 
-                <p>projetos</p>
+                {projects.length > 0 &&
+                projects.map((project) => 
+                <ProjectCard 
+                id={project.id}
+                name={project.name}
+                budget={project.budget}
+                key={project.category.id}
+                category={project.category.name}
+                
+                
+                />)}
             </Container>
+        
         </div>
     )
 }
